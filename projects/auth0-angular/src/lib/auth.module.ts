89982f066b2a7d78e0df7e5ec73fb8f502c9664c
Steve Hobbs <steve.hobbs.mail@gmail.com>
2020-07-09 16:45:57 +0100

    Add library, rename types + imports

---
+++ projects/auth0-angular/src/lib/auth.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [],
+  providers: [AuthService],
+})
+export class AuthModule {}
---
+++ projects/auth0-angular/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of auth0-angular
+ */
+
+export * from './lib/auth.service';
+export * from './lib/auth.module';
