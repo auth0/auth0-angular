97c3673e4a2d6f0ddab4e525c192991d5926aab6
Steve Hobbs <steve.hobbs.mail@gmail.com>
2020-07-29 12:15:34 +0100

    Refactor interceptor into observables

--- projects/auth0-angular/src/lib/auth.interceptor.ts
+++ projects/auth0-angular/src/lib/auth.interceptor.ts
@@ -4,7 +4,7 @@ import {
   HttpHandler,
   HttpEvent,
 } from '@angular/common/http';
-import { Observable, from, of } from 'rxjs';
+import { Observable, from, of, combineLatest, iif } from 'rxjs';
 import { Injectable, Inject } from '@angular/core';
 import {
   AuthConfig,
@@ -13,7 +13,7 @@ import {
 } from './auth.config';
 import { Auth0ClientService } from './auth.client';
 import { Auth0Client } from '@auth0/auth0-spa-js';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map, first, concatMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor {
@@ -22,49 +22,62 @@ export class AuthHttpInterceptor implements HttpInterceptor {
     @Inject(Auth0ClientService) private auth0Client: Auth0Client
   ) {}
 
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (!this.config.httpInterceptor?.allowedList) {
+      return next.handle(req);
+    }
+
+    return this.findMatchingRoute(req).pipe(
+      concatMap((route) =>
+        iif(
+          () => route !== false,
+          from(this.auth0Client.getTokenSilently()).pipe(
+            switchMap((token: string) => {
+              const clone = req.clone({
+                headers: req.headers.set('Authorization', `Bearer ${token}`),
+              });
+
+              return next.handle(clone);
+            })
+          ),
+          next.handle(req)
+        )
+      )
+    );
+  }
+
+  /**
+   * Determines whether the specified route can have an access token attached to it, based on matching the HTTP request against
+   * the interceptor route configuration.
+   * @param route The route to test
+   * @param request The HTTP request
+   */
+  private canAttachToken(
+    route: HttpInterceptorRouteConfig,
+    request: HttpRequest<any>
+  ): boolean {
+    if (route.test instanceof RegExp) {
+      if (route.test.test(request.url)) {
+        return true;
+      }
+    } else if (route.test === request.url) {
+      return true;
+    }
+  }
+
   /**
    * Tries to match a route from the SDK configuration to the HTTP request.
    * If a match is found, the route configuration is returned.
    * @param request The Http request
    */
-  private matchRequest(request: HttpRequest<any>): HttpInterceptorRouteConfig {
-    let routeMatch: HttpInterceptorRouteConfig;
-
-    for (const match of this.config.httpInterceptor.allowedList) {
-      if (match.test instanceof RegExp) {
-        if (match.test.test(request.url)) {
-          routeMatch = match;
-          break;
-        }
-      } else if (match.test === request.url) {
-        routeMatch = match;
-        break;
-      }
-    }
-
-    return routeMatch;
-  }
-
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    if (this.config.httpInterceptor?.allowedList) {
-      const routeMatch = this.matchRequest(req);
-
-      if (routeMatch) {
-        return from(this.auth0Client.getTokenSilently()).pipe(
-          switchMap((token) => {
-            const clone = req.clone({
-              headers: req.headers.set('Authorization', `Bearer ${token}`),
-            });
-
-            return next.handle(clone);
-          })
-        );
-      }
-    }
-
-    return next.handle(req);
+  private findMatchingRoute(
+    request: HttpRequest<any>
+  ): Observable<HttpInterceptorRouteConfig | boolean> {
+    return from(this.config.httpInterceptor.allowedList).pipe(
+      first((route) => this.canAttachToken(route, request), false)
+    );
   }
 }
