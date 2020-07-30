ce037d3ffe307baeb0d78c7a0f10ffdefce4e63c
Steve Hobbs <steve.hobbs.mail@gmail.com>
2020-07-30 14:45:08 +0100

    Refactor req and testData out of individual tests

--- projects/auth0-angular/src/lib/auth.interceptor.spec.ts
+++ projects/auth0-angular/src/lib/auth.interceptor.spec.ts
@@ -1,9 +1,14 @@
 import { AuthHttpInterceptor } from './auth.interceptor';
 import { TestBed, fakeAsync, flush } from '@angular/core/testing';
-import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  HttpClient,
+  HTTP_INTERCEPTORS,
+  HttpRequest,
+} from '@angular/common/http';
 import {
   HttpClientTestingModule,
   HttpTestingController,
+  TestRequest,
 } from '@angular/common/http/testing';
 import { Data } from '@angular/router';
 import { Auth0ClientService } from './auth.client';
@@ -13,6 +18,8 @@ describe('The Auth HTTP Interceptor', () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
   let auth0Client: any;
+  let req: TestRequest;
+  const testData: Data = { message: 'Hello, world' };
 
   beforeEach(() => {
     auth0Client = jasmine.createSpyObj('Auth0Client', ['getTokenSilently']);
@@ -56,19 +63,17 @@ describe('The Auth HTTP Interceptor', () => {
 
   afterEach(() => {
     httpTestingController.verify();
+    req.flush(testData);
   });
 
   describe('Requests that do not require authentication', () => {
     it('pass through and do not have access tokens attached', () => {
-      const testData: Data = { message: 'Hello, world' };
-
       httpClient.get<Data>('/non-api').subscribe((result) => {
         expect(result).toEqual(testData);
         expect(req.request.headers.get('Authorization')).toBeFalsy();
       });
 
-      const req = httpTestingController.expectOne('/non-api');
-      req.flush(testData);
+      req = httpTestingController.expectOne('/non-api');
     });
   });
 
@@ -76,35 +81,27 @@ describe('The Auth HTTP Interceptor', () => {
     it('attach the access token when the configuration uri is a string', fakeAsync((
       done
     ) => {
-      const testData: Data = { message: 'Hello, world' };
-
       httpClient.get('/basic-api').subscribe(done);
       flush();
 
-      const req = httpTestingController.expectOne('/basic-api');
+      req = httpTestingController.expectOne('/basic-api');
 
       expect(req.request.headers.get('Authorization')).toBe(
         'Bearer access-token'
       );
-
-      req.flush(testData);
     }));
 
     it('attach the access token when the configuration uri is a regex', fakeAsync((
       done
     ) => {
-      const testData: Data = { message: 'Hello, world' };
-
       httpClient.get('/basic-api-regex?value=123').subscribe(done);
       flush();
 
-      const req = httpTestingController.expectOne('/basic-api-regex?value=123');
+      req = httpTestingController.expectOne('/basic-api-regex?value=123');
 
       expect(req.request.headers.get('Authorization')).toBe(
         'Bearer access-token'
       );
-
-      req.flush(testData);
     }));
   });
 
@@ -113,45 +110,36 @@ describe('The Auth HTTP Interceptor', () => {
       done
     ) => {
       // Async testing: https://github.com/angular/angular/issues/25733#issuecomment-636154553
-      const testData: Data = { message: 'Hello, world' };
-
       httpClient.get('/api').subscribe(done);
       flush();
 
-      const req = httpTestingController.expectOne('/api');
+      req = httpTestingController.expectOne('/api');
 
       expect(req.request.headers.get('Authorization')).toBe(
         'Bearer access-token'
       );
-
-      req.flush(testData);
     }));
 
     it('attach the access token when the uri is configured using a regex', fakeAsync((
       done
     ) => {
-      const testData: Data = { message: 'Hello, world' };
-
       httpClient.get('/regex-api?my-param=42').subscribe(done);
       flush();
 
-      const req = httpTestingController.expectOne('/regex-api?my-param=42');
+      req = httpTestingController.expectOne('/regex-api?my-param=42');
 
       expect(req.request.headers.get('Authorization')).toBe(
         'Bearer access-token'
       );
-
-      req.flush(testData);
     }));
 
     it('pass through the route options to getTokenSilently, without additional properties', fakeAsync((
       done
     ) => {
-      const testData: Data = { message: 'Hello, world' };
       httpClient.get('/api-with-options').subscribe(done);
       flush();
 
-      const req = httpTestingController.expectOne('/api-with-options');
+      req = httpTestingController.expectOne('/api-with-options');
 
       expect(req.request.headers.get('Authorization')).toBe(
         'Bearer access-token'
@@ -161,8 +149,6 @@ describe('The Auth HTTP Interceptor', () => {
         audience: 'audience',
         scope: 'scope',
       });
-
-      req.flush(testData);
     }));
   });
 });
