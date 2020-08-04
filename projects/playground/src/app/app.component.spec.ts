9f4c3cc90e3d8d4d2527da361d620e4bd6d1647e
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-08-04 19:46:35 -0300

    fix playground tests

--- projects/playground/src/app/app.component.spec.ts
+++ projects/playground/src/app/app.component.spec.ts
@@ -1,17 +1,25 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { AuthService } from 'projects/auth0-angular/src/lib/auth.service';
 import { BehaviorSubject } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   let authMock: AuthService;
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let ne;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     authMock = jasmine.createSpyObj(
       'AuthService',
       {
         loginWithRedirect: jasmine.createSpy().and.returnValue(null),
+        loginWithPopup: jasmine.createSpy().and.returnValue(null),
+        logout: jasmine.createSpy().and.returnValue(null),
+        getAccessTokenSilently: jasmine.createSpy().and.returnValue(null),
+        getAccessTokenWithPopup: jasmine.createSpy().and.returnValue(null),
       },
       {
         user$: new BehaviorSubject(null),
@@ -21,7 +29,7 @@ describe('AppComponent', () => {
     ) as any;
 
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, ReactiveFormsModule],
       declarations: [AppComponent],
       providers: [
         {
@@ -30,108 +38,206 @@ describe('AppComponent', () => {
         },
       ],
     }).compileComponents();
-  }));
 
-  describe('constructor', () => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    ne = fixture.nativeElement;
+    fixture.detectChanges();
+  });
+
+  describe('when initialized', () => {
     it('should create the app', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      expect(app).toBeTruthy();
-    });
-
-    it('should have as title `Auth0 Angular Playground`', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      expect(app.title).toEqual('Auth0 Angular Playground');
+      expect(component).toBeTruthy();
     });
 
     it('should render title', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      const compiled = fixture.nativeElement;
-      expect(compiled.querySelector('h1').textContent).toContain(
-        'AUTH0 ANGULAR PLAYGROUND'
-      );
+      const h1 = ne.querySelector('h1');
+      expect(h1.textContent).toContain('AUTH0 ANGULAR PLAYGROUND');
     });
 
-    it('should render buttons', () => {
-      const fixture = TestBed.createComponent(AppComponent);
+    it('should render SDK loading status', () => {
+      const loadingIndicator = ne.querySelector('p.status-indicator');
+      expect(loadingIndicator.textContent).toContain('SDK initialized = false');
+
+      const loading = authMock.isLoading$ as BehaviorSubject<boolean>;
+      loading.next(false);
       fixture.detectChanges();
-      const compiled = fixture.nativeElement;
 
-      const butLoginRedirect = compiled.querySelector('#login-redirect');
-      expect(butLoginRedirect.textContent).toContain('Log in (redirect)');
+      expect(loadingIndicator.textContent).toContain('SDK initialized = true');
+    });
 
-      const butLoginPopup = compiled.querySelector('#login-popup');
-      expect(butLoginPopup.textContent).toContain('Log in (popup)');
+    it('should show controls when SDK finishes loading', () => {
+      let actions = ne.querySelector('.actions');
+      expect(actions).toBeNull();
 
-      const butLogout = compiled.querySelector('#logout');
-      expect(butLogout.textContent).toContain('Log out');
+      const loading = authMock.isLoading$ as BehaviorSubject<boolean>;
+      loading.next(false);
+      fixture.detectChanges();
 
-      const butLogoutFederated = compiled.querySelector('#logout-federated');
-      expect(butLogoutFederated.textContent).toContain('Log out (federated)');
+      actions = ne.querySelector('.actions');
+      expect(actions).toBeTruthy();
     });
   });
 
   describe('when user is authenticated', () => {
     beforeEach(() => {
-      let authenticated = authMock.isAuthenticated$ as BehaviorSubject<boolean>;
+      const loading = authMock.isLoading$ as BehaviorSubject<boolean>;
+      const authenticated = authMock.isAuthenticated$ as BehaviorSubject<
+        boolean
+      >;
+      const user = authMock.user$ as BehaviorSubject<any>;
+
+      loading.next(false);
       authenticated.next(true);
+      user.next({ name: 'John', lastname: 'Doe' });
+      fixture.detectChanges();
     });
 
-    it('should enable logout buttons', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      const compiled = fixture.nativeElement;
-
-      const butLogout = compiled.querySelector('#logout');
-      expect(butLogout.disabled).toBeFalse();
-
-      const butFederated = compiled.querySelector('#logout-federated');
-      expect(butFederated.disabled).toBeFalse();
+    it('should hide login options', () => {
+      const wrapLogin = ne.querySelector('.login-wrapper');
+      expect(wrapLogin).toBeNull();
     });
 
-    it('should disable login buttons', () => {
-      const fixture = TestBed.createComponent(AppComponent);
+    it('should show logout options', () => {
+      const wrapLogout = ne.querySelector('.logout-wrapper');
+      expect(wrapLogout).toBeTruthy();
+
+      const btnLogout = ne.querySelector('#logout');
+      expect(btnLogout).toBeTruthy();
+    });
+
+    xit('should logout with default options', () => {
+      //TODO: uncheck all checkboxes
+
+      const btnLogout = ne.querySelector('#logout');
+      btnLogout.click();
       fixture.detectChanges();
-      const compiled = fixture.nativeElement;
 
-      const butLogout = compiled.querySelector('#login-popup');
-      expect(butLogout.disabled).toBeTrue();
+      //TODO: assert logout() was called without options
+    });
 
-      const butFederated = compiled.querySelector('#login-redirect');
-      expect(butFederated.disabled).toBeTrue();
+    xit('should logout with federated', () => {
+      //TODO: check federated
+
+      const btnLogout = ne.querySelector('#logout');
+      btnLogout.click();
+      fixture.detectChanges();
+
+      //TODO: assert logout() was called with federated=true
+    });
+
+    xit('should logout with localOnly', () => {
+      //TODO: check localOnly
+
+      const btnLogout = ne.querySelector('#logout');
+      btnLogout.click();
+      fixture.detectChanges();
+
+      //TODO: assert logout() was called with localOnly=true
+    });
+
+    it('should show user profile', () => {
+      const divProfile = ne.querySelectorAll('.artifacts-wrapper .artifact')[0];
+      expect(divProfile.querySelector('p').textContent).toContain(
+        'User Profile: Subset of the ID token claims'
+      );
+      const userValue = JSON.parse(
+        divProfile.querySelector('textarea').textContent
+      );
+      expect(userValue).toEqual({ name: 'John', lastname: 'Doe' });
+    });
+
+    it('should show empty access token', () => {
+      const divToken = ne.querySelectorAll('.artifacts-wrapper .artifact')[1];
+      expect(divToken.querySelector('p').textContent).toContain(
+        'Access Token: Select a mode and click the button to retrieve the token.'
+      );
+      const tokenContent = divToken.querySelector('textarea').textContent;
+      expect(tokenContent).toEqual('');
+    });
+
+    xit('should get access token silently', () => {
+      const divToken = ne.querySelectorAll('.artifacts-wrapper .artifact')[1];
+      expect(divToken.querySelector('p').textContent).toContain('Access Token');
+
+      (authMock.getAccessTokenSilently as jasmine.Spy).and.returnValue(
+        'access token silently'
+      );
+      //TODO: select "silently" radio button
+
+      const btnRefresh = divToken.querySelector('button');
+      btnRefresh.click();
+      fixture.detectChanges();
+      //TODO: assert updateAccessToken() was called?
+
+      const tokenContent = divToken.querySelector('textarea').textContent;
+      expect(tokenContent).toEqual('access token silently');
+    });
+
+    xit('should get access token with popup', () => {
+      const divToken = ne.querySelectorAll('.artifacts-wrapper .artifact')[1];
+      expect(divToken.querySelector('p').textContent).toContain('Access Token');
+
+      (authMock.getAccessTokenWithPopup as jasmine.Spy).and.returnValue(
+        'access token popup'
+      );
+      //TODO: select "popup" radio button
+
+      const btnRefresh = divToken.querySelector('button');
+      btnRefresh.click();
+      fixture.detectChanges();
+      //TODO: assert updateAccessToken() was called?
+
+      const tokenContent = divToken.querySelector('textarea').textContent;
+      expect(tokenContent).toEqual('access token popup');
     });
   });
 
   describe('when user is not authenticated', () => {
     beforeEach(() => {
-      let authenticated = authMock.isAuthenticated$ as BehaviorSubject<boolean>;
+      const loading = authMock.isLoading$ as BehaviorSubject<boolean>;
+      const authenticated = authMock.isAuthenticated$ as BehaviorSubject<
+        boolean
+      >;
+
+      loading.next(false);
       authenticated.next(false);
+      fixture.detectChanges();
     });
 
-    it('should disable logout buttons', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      const compiled = fixture.nativeElement;
-
-      const butLogout = compiled.querySelector('#logout');
-      expect(butLogout.disabled).toBeTrue();
-
-      const butFederated = compiled.querySelector('#logout-federated');
-      expect(butFederated.disabled).toBeTrue();
+    it('should hide logout options', () => {
+      const wrapLogout = ne.querySelector('.logout-wrapper');
+      expect(wrapLogout).toBeNull();
     });
 
-    it('should enable login buttons', () => {
-      const fixture = TestBed.createComponent(AppComponent);
+    it('should show login options', () => {
+      const wrapLogin = ne.querySelector('.login-wrapper');
+      expect(wrapLogin).toBeTruthy();
+
+      const btnLogin = ne.querySelector('#login');
+      expect(btnLogin).toBeTruthy();
+    });
+
+    xit('should login with redirect', () => {
+      const wrapLogin = ne.querySelector('.login-wrapper');
+
+      //TODO: select "redirect" radio button
+
+      const btnRefresh = wrapLogin.querySelector('button');
+      btnRefresh.click();
       fixture.detectChanges();
-      const compiled = fixture.nativeElement;
+      //TODO: assert loginWithRedirect() was called?
+    });
 
-      const butLogout = compiled.querySelector('#login-popup');
-      expect(butLogout.disabled).toBeFalse();
+    xit('should login with popup', () => {
+      const wrapLogin = ne.querySelector('.login-wrapper');
 
-      const butFederated = compiled.querySelector('#login-redirect');
-      expect(butFederated.disabled).toBeFalse();
+      //TODO: select "popup" radio button
+
+      const btnRefresh = wrapLogin.querySelector('button');
+      btnRefresh.click();
+      fixture.detectChanges();
+      //TODO: assert loginWithPopup() was called?
     });
   });
 });
