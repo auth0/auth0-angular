b2eacf189afc7c513d17784c616f3450e5649c77
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-09-04 14:32:19 -0300

    improve the e2e tests

--- projects/playground/e2e/integration/playground.spec.ts
+++ projects/playground/e2e/integration/playground.spec.ts
@@ -8,9 +8,21 @@ if (!EMAIL || !PASSWORD) {
 }
 
 const loginToAuth0 = () => {
-  cy.get('.auth0-lock-input-username .auth0-lock-input').clear().type(EMAIL);
-  cy.get('.auth0-lock-input-password .auth0-lock-input').clear().type(PASSWORD);
-  cy.get('.auth0-lock-submit').click();
+  cy.get('.auth0-lock-form')
+    .should('have.length.above', 1)
+    .then(($form) => {
+      if ($form.find('.auth0-lock-last-login-pane').length) {
+        cy.get('.auth0-lock-last-login-pane > a').click();
+        return;
+      }
+      cy.get('.auth0-lock-input-username .auth0-lock-input')
+        .clear()
+        .type(EMAIL);
+      cy.get('.auth0-lock-input-password .auth0-lock-input')
+        .clear()
+        .type(PASSWORD);
+      cy.get('.auth0-lock-submit').click();
+    });
 };
 
 describe('Smoke tests', () => {
@@ -23,8 +35,7 @@ describe('Smoke tests', () => {
 
   it('shows default logged in options', () => {
     cy.visit('/');
-    cy.get('#login').should('be.visible');
-    cy.get('#login').click();
+    cy.get('#login').should('be.visible').click();
 
     loginToAuth0();
 
@@ -34,14 +45,12 @@ describe('Smoke tests', () => {
     cy.get('[data-cy=accessToken-silently]').should('be.checked');
     cy.get('[data-cy=accessToken-popup]').should('not.be.checked');
 
-    cy.get('#logout').should('be.visible');
-    cy.get('#logout').click();
+    cy.get('#logout').should('be.visible').click();
   });
 
   it('do redirect login and show user and access token', () => {
     cy.visit('/');
-    cy.get('#login').should('be.visible');
-    cy.get('#login').click();
+    cy.get('#login').should('be.visible').click();
 
     cy.url().should('include', 'https://brucke.auth0.com/login');
     loginToAuth0();
@@ -50,25 +59,21 @@ describe('Smoke tests', () => {
 
     cy.get('[data-cy=accessToken]').should('be.empty');
     cy.get('#accessToken').click();
-    cy.wait(500);
     cy.get('[data-cy=accessToken]')
+      .should('not.be.empty')
       .invoke('text')
       .then((token) => {
-        // tslint:disable-next-line: no-unused-expression
-        expect(token).not.to.be.empty;
         cy.get('#accessToken').click();
         cy.get('[data-cy=accessToken]').should('have.text', token);
       });
 
-    cy.get('#logout').should('be.visible');
-    cy.get('#logout').click();
-    cy.get('#login').should('exist');
+    cy.get('#logout').should('be.visible').click();
+    cy.get('#login').should('be.visible');
   });
 
   it('do redirect login and get new access token', () => {
     cy.visit('/');
-    cy.get('#login').should('be.visible');
-    cy.get('#login').click();
+    cy.get('#login').should('be.visible').click();
 
     cy.url().should('include', 'https://brucke.auth0.com/login');
     loginToAuth0();
@@ -78,68 +83,56 @@ describe('Smoke tests', () => {
     cy.get('[data-cy=accessToken]').should('be.empty');
     cy.get('[data-cy=accessToken-ignoreCache]').check();
     cy.get('#accessToken').click();
-    cy.wait(2000);
     cy.get('[data-cy=accessToken]')
+      .should('not.be.empty')
       .invoke('text')
       .then((token) => {
-        // tslint:disable-next-line: no-unused-expression
-        expect(token).not.to.be.empty;
         cy.get('#accessToken').click();
-        cy.get('[data-cy=accessToken]').should('not.be.empty');
-        cy.get('[data-cy=accessToken]').should('not.have.text', token);
+        cy.get('[data-cy=accessToken]')
+          .should('not.be.empty')
+          .and('not.have.text', token);
       });
 
-    cy.get('#logout').should('be.visible');
-    cy.get('#logout').click();
-    cy.get('#login').should('exist');
+    cy.get('#logout').should('be.visible').click();
+    cy.get('#login').should('be.visible');
   });
 
-  it('do local logout', () => {
+  it('do local logout (auth0 session remains valid)', () => {
     cy.visit('/');
-    cy.get('#login').should('be.visible');
-    cy.get('#login').click();
+    cy.get('#login').should('be.visible').click();
 
     cy.url().should('include', 'https://brucke.auth0.com/login');
     loginToAuth0();
 
     cy.get('[data-cy=logout-localOnly]').check();
-
     cy.get('#logout').click();
 
-    cy.get('#login').should('be.visible');
-    cy.get('#login').click();
-
+    cy.get('#login').should('be.visible').click();
     cy.url().should('include', 'https://brucke.auth0.com/login');
-    cy.get('.auth0-lock-last-login-pane').should('exist');
-    cy.get('.auth0-lock-last-login-pane > a').click();
+    // logs in with the "last time you logged in with" button
+    cy.get('.auth0-lock-last-login-pane > a').should('be.visible').click();
 
-    cy.get('#logout').should('be.visible');
-    cy.get('#logout').click();
-    cy.get('#login').should('exist');
+    cy.get('#logout').should('be.visible').click();
+    cy.get('#login').should('be.visible');
   });
 
-  it('do federated logout', () => {
+  it('do federated logout (auth0 session is cleared)', () => {
     cy.visit('/');
-    cy.get('#login').should('be.visible');
-    cy.get('#login').click();
+    cy.get('#login').should('be.visible').click();
 
     cy.url().should('include', 'https://brucke.auth0.com/login');
     loginToAuth0();
 
     cy.get('[data-cy=logout-federated]').check();
-
     cy.get('#logout').click();
 
-    cy.get('#login').should('be.visible');
-    cy.get('#login').click();
-
+    cy.get('#login').should('be.visible').click();
     cy.url().should('include', 'https://brucke.auth0.com/login');
-    cy.get('.auth0-lock-last-login-pane').should('not.exist');
+    cy.get('.auth0-lock-last-login-pane').should('not.be.visible');
     loginToAuth0();
 
-    cy.get('#logout').should('be.visible');
-    cy.get('#logout').click();
-    cy.get('#login').should('exist');
+    cy.get('#logout').should('be.visible').click();
+    cy.get('#login').should('be.visible');
   });
 
   it('should protect a route and return to path after login', () => {
