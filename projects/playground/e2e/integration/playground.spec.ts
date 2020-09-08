6f73577a1d52e123285963df0769fe098d82313b
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-09-08 06:29:15 -0300

    Add e2e tests for the playground app (#43)
    * add cypress tests
    
    * update angular builder scripts to run e2e with cypress
    
    * ignore chai assertion lint warning
    
    * improve the e2e tests
    
    * refactor ci script to cache both .npm and .cache folders
    
    * rename regular logout test

---
+++ projects/playground/e2e/integration/playground.spec.ts
@@ -0,0 +1,157 @@
+const EMAIL = Cypress.env('USER_EMAIL');
+const PASSWORD = Cypress.env('USER_PASSWORD');
+
+if (!EMAIL || !PASSWORD) {
+  throw new Error(
+    'You must provide CYPRESS_USER_EMAIL and CYPRESS_USER_PASSWORD environment variables'
+  );
+}
+
+const loginToAuth0 = () => {
+  cy.get('.auth0-lock-form')
+    .should('have.length.above', 1)
+    .then(($form) => {
+      if ($form.find('.auth0-lock-last-login-pane').length) {
+        cy.get('.auth0-lock-last-login-pane > a').click();
+        return;
+      }
+      cy.get('.auth0-lock-input-username .auth0-lock-input')
+        .clear()
+        .type(EMAIL);
+      cy.get('.auth0-lock-input-password .auth0-lock-input')
+        .clear()
+        .type(PASSWORD);
+      cy.get('.auth0-lock-submit').click();
+    });
+};
+
+describe('Smoke tests', () => {
+  it('shows default logged out options', () => {
+    cy.visit('/');
+    cy.get('#login').should('be.visible');
+    cy.get('[data-cy=login-redirect]').should('be.checked');
+    cy.get('[data-cy=login-popup]').should('not.be.checked');
+  });
+
+  it('shows default logged in options', () => {
+    cy.visit('/');
+    cy.get('#login').should('be.visible').click();
+
+    loginToAuth0();
+
+    cy.get('[data-cy=logout-local]').should('not.be.checked');
+    cy.get('[data-cy=logout-federated]').should('not.be.checked');
+    cy.get('[data-cy=accessToken-ignoreCache]').should('not.be.checked');
+    cy.get('[data-cy=accessToken-silently]').should('be.checked');
+    cy.get('[data-cy=accessToken-popup]').should('not.be.checked');
+
+    cy.get('#logout').should('be.visible').click();
+  });
+
+  it('do redirect login and show user and access token', () => {
+    cy.visit('/');
+    cy.get('#login').should('be.visible').click();
+
+    cy.url().should('include', 'https://brucke.auth0.com/login');
+    loginToAuth0();
+
+    cy.get('[data-cy=userProfile]').contains(`"email": "${EMAIL}"`);
+
+    cy.get('[data-cy=accessToken]').should('be.empty');
+    cy.get('#accessToken').click();
+    cy.get('[data-cy=accessToken]')
+      .should('not.be.empty')
+      .invoke('text')
+      .then((token) => {
+        cy.get('#accessToken').click();
+        cy.get('[data-cy=accessToken]').should('have.text', token);
+      });
+
+    cy.get('#logout').should('be.visible').click();
+    cy.get('#login').should('be.visible');
+  });
+
+  it('do redirect login and get new access token', () => {
+    cy.visit('/');
+    cy.get('#login').should('be.visible').click();
+
+    cy.url().should('include', 'https://brucke.auth0.com/login');
+    loginToAuth0();
+
+    cy.get('[data-cy=userProfile]').contains(`"email": "${EMAIL}"`);
+
+    cy.get('[data-cy=accessToken]').should('be.empty');
+    cy.get('[data-cy=accessToken-ignoreCache]').check();
+    cy.get('#accessToken').click();
+    cy.get('[data-cy=accessToken]')
+      .should('not.be.empty')
+      .invoke('text')
+      .then((token) => {
+        cy.get('#accessToken').click();
+        cy.get('[data-cy=accessToken]')
+          .should('not.be.empty')
+          .and('not.have.text', token);
+      });
+
+    cy.get('#logout').should('be.visible').click();
+    cy.get('#login').should('be.visible');
+  });
+
+  it('do local logout (auth0 session remains valid)', () => {
+    cy.visit('/');
+    cy.get('#login').should('be.visible').click();
+
+    cy.url().should('include', 'https://brucke.auth0.com/login');
+    loginToAuth0();
+
+    cy.get('[data-cy=logout-localOnly]').check();
+    cy.get('#logout').click();
+
+    cy.get('#login').should('be.visible').click();
+    cy.url().should('include', 'https://brucke.auth0.com/login');
+    // logs in with the "last time you logged in with" button
+    cy.get('.auth0-lock-last-login-pane > a').should('be.visible').click();
+
+    cy.get('#logout').should('be.visible').click();
+    cy.get('#login').should('be.visible');
+  });
+
+  it('do regular logout (auth0 session is cleared)', () => {
+    cy.visit('/');
+    cy.get('#login').should('be.visible').click();
+
+    cy.url().should('include', 'https://brucke.auth0.com/login');
+    loginToAuth0();
+
+    cy.get('#logout').click();
+
+    cy.get('#login').should('be.visible').click();
+    cy.url().should('include', 'https://brucke.auth0.com/login');
+    cy.get('.auth0-lock-last-login-pane').should('not.be.visible');
+    loginToAuth0();
+
+    cy.get('#logout').should('be.visible').click();
+    cy.get('#login').should('be.visible');
+  });
+
+  it('should protect a route and return to path after login', () => {
+    cy.visit('/');
+    cy.get('[data-cy=protected]').should('not.be.visible');
+    cy.visit('/protected');
+
+    cy.url().should('include', 'https://brucke.auth0.com/login');
+    loginToAuth0();
+
+    cy.url().should('include', '/protected');
+    cy.get('[data-cy=protected]').should('be.visible');
+    cy.get('#logout').click();
+  });
+
+  it('should see public route content without logging in', () => {
+    cy.visit('/');
+    cy.get('#login').should('be.visible');
+
+    cy.get('[data-cy=unprotected]').should('be.visible');
+    cy.get('[data-cy=protected]').should('not.be.visible');
+  });
+});
