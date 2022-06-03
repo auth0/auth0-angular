a67cd5d395fa7312beed90e530d228823195c2e9
Frederik <frederik.prijck@gmail.com>
2022-06-03 09:57:58 +0200

    Add tests

--- projects/auth0-angular/src/lib/auth.client.spec.ts
+++ projects/auth0-angular/src/lib/auth.client.spec.ts
@@ -22,5 +22,42 @@ describe('Auth0ClientFactory', () => {
         /^Configuration must be specified/
       );
     });
+
+    it('creates a new instance of Auth0Client with the correct properties to skip the refreshtoken fallback', () => {
+      const config: AuthConfig = {
+        domain: 'test.domain.com',
+        clientId: 'abc123',
+        useRefreshTokens: true,
+        useRefreshTokensFallback: false,
+      };
+
+      const configClient = new AuthClientConfig(config);
+      const client = Auth0ClientFactory.createClient(configClient);
+
+      expect(client).not.toBeUndefined();
+      expect((client as any).options.domain).toEqual('test.domain.com');
+      expect((client as any).options.client_id).toEqual('abc123');
+      expect((client as any).options.useRefreshTokens).toEqual(true);
+      expect((client as any).options.useRefreshTokensFallback).toEqual(false);
+    });
+
+    it('creates a new instance of Auth0Client with the correct properties without specifying any value for useRefreshTokensFallback', () => {
+      const config: AuthConfig = {
+        domain: 'test.domain.com',
+        clientId: 'abc123',
+        useRefreshTokens: true,
+      };
+
+      const configClient = new AuthClientConfig(config);
+      const client = Auth0ClientFactory.createClient(configClient);
+
+      expect(client).not.toBeUndefined();
+      expect((client as any).options.domain).toEqual('test.domain.com');
+      expect((client as any).options.client_id).toEqual('abc123');
+      expect((client as any).options.useRefreshTokens).toEqual(true);
+      expect(
+        (client as any).options.useRefreshTokensFallback
+      ).not.toBeDefined();
+    });
   });
 });
