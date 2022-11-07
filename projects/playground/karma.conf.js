8f775c3abf5c3ab2b7a02e1f061e12885870f5fe
frederikprijck <frederik.prijck@auth0.com>
2022-11-07 17:16:37 +0100

    fix coverage path

--- projects/playground/karma.conf.js
+++ projects/playground/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './../../coverage/auth0-angular'),
+      dir: require('path').join(__dirname, './../../coverage/playground'),
       subdir: '.',
       reporters: [
         { type: 'html' },
