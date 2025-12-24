286b64a48b8a9152d0336caa5534daaaa4145c11
Yogesh Chaudhary <yogesh.chaudhary@okta.com>
2025-12-24 11:06:44 +0530

    fix: add RouterOutlet import to ChildRouteComponent

--- projects/playground/src/app/components/child-route.component.ts
+++ projects/playground/src/app/components/child-route.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-child-route',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
     <router-outlet></router-outlet>
   `,
   styles: [],
+  imports: [RouterOutlet],
 })
 export class ChildRouteComponent {}
